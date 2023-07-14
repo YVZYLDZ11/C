6fe2da9672ae49ab913ea5e7b6bc8e69d8544054
Yavuz <yvzyldz20@gmail.com>
2023-07-14 14:36:44 +0300

    C

---
+++ HelloWorld/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#int main()
+#{
+    #printf("Hello world!\n");
+    #return 0;
+#}
+int main()
+{
+    printf("   /|\n");
+    printf("  / |\n");
+    printf(" /  |\n");
+    printf("/___|\n");
+    return 0;
+}
