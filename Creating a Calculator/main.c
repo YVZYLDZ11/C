419ab978afbf0625ca27daa517de3361349e34df
Yavuz <yvzyldz20@gmail.com>
2023-07-22 21:46:58 +0300

    C

---
+++ Creating a Calculator/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    double num1, num2;
+
+    printf("num1: ");
+    scanf("%lf", &num1);
+
+    printf("num2: ");
+    scanf("%lf", &num2);
+
+    printf("%f", num1 * num2);
+
+    return 0;
+}
