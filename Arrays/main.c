4f3c2c73193f9b3bfe1b35a56d12638afb432f60
Yavuz <yvzyldz20@gmail.com>
2023-08-13 12:29:46 +0300

    Arrays

---
+++ Arrays/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*int main()
+{
+    int luckyNumbers[] = {4,8,15,23,42};
+    luckyNumbers[1] = 200;
+    printf("%d", luckyNumbers[1]);
+
+    return 0;
+}
+*/
+
+int main()
+{
+    int luckyNumbers[10];
+    luckyNumbers[1] = 80;
+    luckyNumbers[0] = 90;
+    printf("%d", luckyNumbers[0]);
+
+    return 0;
+}
